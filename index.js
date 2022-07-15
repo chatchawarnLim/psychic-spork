bca98028e2999aaef3dd9f887b8ecb419262888b
Glitch (plume-darkened-brazil) <none>
2022-07-15 23:32:25 +0000

    🎻🏂 Checkpoint ./index.js:46660815/857 ./package.json:46660815/91

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+var bodyParser = require("body-parser");
+
+
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const urlSchema = mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: Number,
+});
+
+let Url = mongoose.model("url", urlSchema);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(cors())
 app.use(express.static('public'))

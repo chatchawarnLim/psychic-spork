7eb3a68914d50cf857e984e2975f1f212b2cc458
Glitch (plume-darkened-brazil) <none>
2022-07-16 00:08:23 +0000

    🍈🚏 Checkpoint ./index.js:46660815/3249

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 var bodyParser = require("body-parser");
-
 
 let mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
@@ -12,7 +11,15 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 const exerciseTrackSchema = mongoose.Schema({
- 
+  username: String,
+  count: Number,
+  logs: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
 });
 
 let exerciseTracker = mongoose.model("exercise", exerciseTrackSchema);
@@ -23,25 +30,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
+app.get("/api/users", (req, res) => {
+  exerciseTracker.find({}).
+})
 
-
-
-app.get('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   //res.sendFile(__dirname + '/views/index.html')
   let body = req.body;
-  if('username' in body){
+  if ("username" in body) {
     // do the save
+    let newUser = new exerciseTracker({ username: body.username });
+    newUser.save(function (err, data) {
+      if (err) return res.sent(err.message);
+      console.log(data)
+      res.json({"_id":data._id, "username": data.username});
+    });
   }
 });
 
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});

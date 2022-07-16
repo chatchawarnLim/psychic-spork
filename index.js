4890946b73aba0c6ccc71cff5595d16075d7346c
Glitch (plume-darkened-brazil) <none>
2022-07-16 01:06:44 +0000

    🚥🏄 Checkpoint ./index.js:46660815/6120

--- index.js
+++ index.js
@@ -43,62 +43,59 @@ app.post("/api/users", (req, res) => {
     // do the save
     let newUser = new exerciseTracker({ username: body.username });
     newUser.save(function (err, data) {
-      console.log(err)
+      console.log(err);
       if (err) return res.sent(err.message);
-      res.json({"_id":data._id, "username": data.username});
+      res.json({ _id: data._id, username: data.username });
     });
   }
-}); 
+});
 
 app.get("/api/users/:id/logs", (req, res) => {
-  let queryParam = req.params
+  let queryParam = req.params;
 
-  if(!'id' in queryParam) res.status(404).send("Do ")
+  if (!"id" in queryParam) res.status(404).send("Do ");
 
-  exerciseTracker.findById(queryParam.id , (err,exerciseTrackerData) => {
-    if(err) res.sent(err)
-    console.log(exerciseTrackerData)
-    exerciseTrackerData.count =exerciseTrackerData.logs.length
-    res.json(exerciseTrackerData)
-    
-  })
-
+  exerciseTracker.findById(queryParam.id, (err, exerciseTrackerData) => {
+    if (err) res.sent(err);
+    console.log(exerciseTrackerData);
+    exerciseTrackerData.count = exerciseTrackerData.logs.length;
+    res.json(exerciseTrackerData);
+  });
 });
-
 
 app.post("/api/users/:_id/exercises", (req, res) => {
-  let queryParam = req.params
-  let body = req.body
+  let queryParam = req.params;
+  let body = req.body;
 
-  if(!'_id' in queryParam) res.status(404).send("Do ")
-  if(!'date' in req.body){
-    body.date = new Date()
+  if (!"_id" in queryParam) res.status(404).send("Do ");
+  if (!"date" in req.body) {
+    body.date = new Date();
   }
-  
-  exerciseTracker.findById(queryParam._id , (err,exerciseTrackerData) => {
-    if(err) res.sent(err)
-    console.log("post ext",exerciseTrackerData)
-    exerciseTrackerData.logs.push(body)
-    body['_id'] = queryParam._id;
-    body['username'] = exerciseTrackerData.username
-    exerciseTrackerData.save((err, updateExercise)=>{
-      if(err) return console.log(err)
-      console.log("post ext",updateExercise)
-      res.json(updateExercise)
-    })
-    
-  })
 
+  exerciseTracker.findById(queryParam._id, (err, exerciseTrackerData) => {
+    if (err) res.sent(err);
+    console.log("body", body)
+    console.log("post ext", exerciseTrackerData);
+    exerciseTrackerData.logs.push(body);
+    console.log("post update ext", exerciseTrackerData);
+    /*
+    exerciseTracker
+      .findOneAndUpdate({ id: queryParam._id }, {logs : exerciseTrackerData.logs}, {
+        new: true,
+      })
+      .then((data) => {
+        if (err) return console.log(err);
+        console.log("post ext", data);
+        res.json(data);
+      });*/
+  });
 });
 
-
 app.get("/api/users", (req, res) => {
-  exerciseTracker.find({}).then(function(data){
-    res.json(data)
-  })
-})
-
-
+  exerciseTracker.find({}).then(function (data) {
+    res.json(data);
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
